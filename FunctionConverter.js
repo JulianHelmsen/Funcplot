741da5166025afc47b26a234b871a55903d8cc46
Brukey <amBrukey>
2021-02-12 16:54:47 +0100

    FunctionConverter.js works

--- FunctionConverter.js
+++ FunctionConverter.js
@@ -1,35 +1,11 @@
 
 
 function convertToFunction(xValue, funcText){
-	let y = funcText;
-	
-	let repeat = false;
-	
-		if(y.includes("x") ){
-			y = y.replace("x", xValue);
-			repeat = true;
-		}	
-		
-		if(y.includes("sin") ){
-			y = y.replace("sin", "Math.sin");
-			repeat = true;
-		}
-
-		if(y.includes("cos") ){
-			y = y.replace("cos", "Math.cos");
-			repeat = true;
-		}
-		
-		if(y.includes("tan") ){
-			y = y.replace("tan", "Math.tan");
-			repeat = true;
-		}
-		
-		if(repeat){
-			convertToFunction(xValue, y);
-		}	
-	
-	return eval(y);
+    funcText = funcText.replaceAll("x", xValue);
+    funcText = funcText.replaceAll("sin", "Math.sin");
+    funcText = funcText.replaceAll("cos", "Math.cos");
+    funcText = funcText.replaceAll("tan", "Math.tan");
+    return funcText;
 }
 
 function testConverter(xValue, funcText){
