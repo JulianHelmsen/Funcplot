3e082820c1bf008703ed9c9f3e021204c964d6a8
Brukey <amBrukey>
2021-02-12 16:39:23 +0100

    version that does not function

--- FunctionConverter.js
+++ FunctionConverter.js
@@ -3,24 +3,31 @@
 function convertToFunction(xValue, funcText){
 	let y = funcText;
 	
+	let repeat = false;
+	
 		if(y.includes("x") ){
 			y = y.replace("x", xValue);
+			repeat = true;
 		}	
 		
 		if(y.includes("sin") ){
 			y = y.replace("sin", "Math.sin");
+			repeat = true;
 		}
 
 		if(y.includes("cos") ){
 			y = y.replace("cos", "Math.cos");
+			repeat = true;
 		}
 		
 		if(y.includes("tan") ){
 			y = y.replace("tan", "Math.tan");
+			repeat = true;
 		}
 		
-	
-	
+		if(repeat){
+			convertToFunction(xValue, y);
+		}	
 	
 	return eval(y);
 }
