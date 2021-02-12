6ef203d04cdfde5fd2f3fd11488ca634e2804a1e
Brukey <amr.al-khwlani@web.de>
2021-02-12 18:49:42 +0100

    FunctionConverter can create an array of points of a function

--- FunctionConverter.js
+++ FunctionConverter.js
@@ -7,12 +7,40 @@ function convertToFunction(funcText){
 
 }
 
+function calculateFunction(xmin, xmax, funcText){
+	let xdelta = xmax - xmin;
+	let point = {x:0, y:0};
+	let pointList = {point, };
+	
+	let xValue = 0.0;
+	let precision = 1000;
+	
+	for(let i = 0; i < precision; i++ ){
+		
+		xValue = i * xdelta / precision + xmin;
+		yValue = convertToFunction(funcText).replaceAll("x", xValue);
+		
+		point.x = xValue;
+		point.y = eval(yValue);
+	
+
+		pointList[i] = point;
+		window.alert(pointList[i].x + "/" + pointList[i].y);
+	}
+	
+	return pointList;
+}
+
 function testConverter(funcText){
-
-
-	let y = eval (convertToFunction(funcText) );
-	let y = convertToFunction(funcText) ;
-
+	
+	let points = calculateFunction(-10, 10, funcText);
+	
+	let y = "";
+	
+	for(let p in points){
+		y += "P( " + p + " / " + p  +" )";
+	}
+	
 	let p = document.getElementById("test");
 	p.innerHTML = y;
 	
